b355bc255db3a9f5083b21b342e904e8be99f2e6
siwachs <ssiwach.8888@gmail.com>
2024-10-14 10:58:41 +0530

    Task Runner

--- src/Questions/TaskRunner.js
+++ src/Questions/TaskRunner.js
@@ -2,16 +2,53 @@
 // Ex: We have 5 task and given limit is 3 so we can oly run 3 task at a time
 
 function createTask(id, delay) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      console.log(`Task ${id} is completed.`);
-      resolve(`Result of Task ${id} is resolved.`);
-    }, delay);
-  });
+  return () => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        console.log(`Task ${id} is completed.`);
+        resolve(`Result of Task ${id} is resolved.`);
+      }, delay);
+    });
+  };
 }
+
+// NOTE: In Event loop if two methods execute first it is hard to predict which are going to execute first it depend on factors -> The wait time does not need to execute method whenever timout finished it is minimum wait time.
+// Means it could eexecute after that minimum timout depending on what is eventloop doing at that time
 
 class TaskRunner {
-  constructor(concurrency) {}
+  constructor(concurrency) {
+    this.concurrency = concurrency;
+    this.runningTasks = 0;
+    this.queue = [];
+  }
 
-  push(task) {}
+  async push(task) {
+    if (this.runningTasks < this.concurrency) await this.execute(task);
+    else this.queue.push(task);
+  }
+
+  async execute(task) {
+    this.runningTasks++;
+
+    try {
+      await task();
+    } catch (error) {
+      console.log(JSON.stringify(error.message, null, 2));
+    } finally {
+      this.runningTasks--;
+
+      if (this.queue.length && this.runningTasks < this.concurrency) {
+        const nextTask = this.queue.shift();
+
+        this.execute(nextTask);
+      }
+    }
+  }
 }
+
+const taskRunner = new TaskRunner(3);
+taskRunner.push(createTask(1, 2000));
+taskRunner.push(createTask(2, 1000));
+taskRunner.push(createTask(3, 1200));
+taskRunner.push(createTask(4, 1000));
+taskRunner.push(createTask(5, 1600));
