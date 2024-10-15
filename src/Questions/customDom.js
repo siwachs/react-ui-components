515fbe1fc3d99bc4540f787a544fb6dd7455b601
siwachs <ssiwach.8888@gmail.com>
2024-10-15 13:22:08 +0530

    customDom

--- src/Questions/customDom.js
+++ src/Questions/customDom.js
@@ -30,6 +30,13 @@ class VDocument extends Node {
 
       let output = "";
       output += `${whitespaces}<${currentNode.name}>\n`;
+
+      if (currentNode.innerHTML)
+        output += `${whitespaces}${getWhitespaces(INDENT_SIZE)}${currentNode.innerHTML}\n`;
+
+      for (const child of currentNode.children)
+        output += printTree(child, currentLevel + 1);
+
       output += `${whitespaces}</${currentNode.name}>\n`;
 
       return output;
@@ -47,5 +54,14 @@ vDocument.appendChild(body);
 const div = vDocument.createElement("div");
 div.innerHTML = "HTML text render inside div.";
 body.appendChild(div);
+
+const div2 = vDocument.createElement("div");
+const div3 = vDocument.createElement("div");
+
+div2.innerHTML = "HTML text in div2. Inner Div of Div 1";
+div3.innerHTML = "HTML text in div3.";
+
+div.appendChild(div2);
+body.appendChild(div3);
 
 vDocument.render();
